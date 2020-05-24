eca4227b1df4a1d898a6e36562a89614f518ec2b
AnandaAp <42529423+AnandaAp@users.noreply.github.com>
2020-05-24 21:05:10 +0700

    Deleting commented code

--- src/Sources/Main.java
+++ src/Sources/Main.java
@@ -6,16 +6,6 @@ public class Main {
 	public static String User;
 	public static void main(String[] args) throws Exception {
 		UserWallet r = new UserWallet();
-//		Wallet w = new Wallet();
-//		w.tambahUser(r);
-//		w.printAccount();
-//		w.addSaldo(new IDR(500000));
-//		w.printAccount();
-//		w.addSaldo(new USD(30));
-//		w.printAccount();
-//		Login login = new Login();
-//		Login.Login();
-//		Wallet w = new Wallet();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
