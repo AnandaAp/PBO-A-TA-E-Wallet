af4c4b6ab8ebb21c737916456b722056d38e0f2a
Yohanesrinto <63203011+yohanesrinto@users.noreply.github.com>
2020-05-25 18:01:15 +0700

    Improve UI and Adding more Input Handling

--- src/Sources/AddBalance.java
+++ src/Sources/AddBalance.java
@@ -1,4 +1,7 @@
 package Sources;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.io.BufferedReader;
 import java.io.File;
@@ -18,12 +21,22 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+
 import javax.swing.JTextField;
+import javax.swing.border.AbstractBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.geom.Area;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.event.ActionEvent;
-
-public class AddBalance {
+import java.util.regex.*;
+public class AddBalance extends AbstractBorder{
 
 	public JFrame frame;
 	private JTextField textField;
@@ -31,11 +44,44 @@ public class AddBalance {
 	private JButton btnNewButton;
 	private JButton btnNewButton_1;
 	public String name,password, email, brth, address, balance;
-	
-
+	private Color color;
+    private int thickness = 4;
+    private int radii = 8;
+    private int pointerSize = 7;
+    private Insets insets = null;
+    private BasicStroke stroke = null;
+    private int strokePad;
+    private int pointerPad = 4;
+    RenderingHints hints;
+    private String numPat = "\\d+";
+    
+    /**
+	 * @wbp.parser.constructor
+	 */
 	public AddBalance(UserWallet u) {
-		initialize(u);
+		this.initialize(u);
 	}
+	public AddBalance(Color color) {
+		this(color, 4, 8, 7);
+	}
+	public AddBalance(
+		    Color color, int thickness, int radii, int pointerSize) {
+	        this.thickness = thickness;
+	        this.radii = radii;
+	        this.pointerSize = pointerSize;
+		    this.color = color;
+		
+		    stroke = new BasicStroke(thickness);
+		    strokePad = thickness/2;
+		
+		    hints = new RenderingHints(
+		        RenderingHints.KEY_ANTIALIASING,
+		        RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		    int pad = radii + strokePad;
+		    int bottomPad = pad + pointerSize + strokePad;
+		    insets = new Insets(pad,pad,bottomPad,pad);
+		}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -58,7 +104,8 @@ public class AddBalance {
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(24, 90, 180, 20);
+		textField.setBounds(24, 90, 180, 28);
+		textField.setBorder(new Register(Color.black.darker(),2,6,0));
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
@@ -68,7 +115,8 @@ public class AddBalance {
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(226, 90, 180, 20);
+		textField_1.setBounds(226, 90, 180, 28);
+		textField_1.setBorder(new Register(Color.black.darker(),2,6,0));
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
@@ -77,6 +125,7 @@ public class AddBalance {
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				AddBalanceFunction(textField,textField_1);
+				JOptionPane.showMessageDialog(frame, "Transaksi Berhasil", "Input Success", JOptionPane.PLAIN_MESSAGE);
 			}
 
 		});
@@ -140,127 +189,193 @@ public class AddBalance {
 				e.printStackTrace();
 			}
 		
-		//balance type double 
-		double balance1 = Double.parseDouble(balance);
-		
-		
-		IDR idr2 = new IDR(balance1);
-		//IDR check
-		if(!idr.isEmpty()&& usd.isEmpty()) {
-		  
-			double idr1 =Double.parseDouble(idr);
-			if(idr1 > 0) {
-				idr2.addValue(idr1);
-				File f2 = new File("profile/"+Main.User+".txt");
-				File file = new File("profile/temp.txt");
+		Pattern pat = Pattern.compile(numPat);
+		Matcher mat = pat.matcher(balance);
+		boolean ok = mat.matches();
+		if(ok) {
+			//balance type double 
+			double balance1 = Double.parseDouble(balance);
 			
-	//Overwrite file	
+			
+			IDR idr2 = new IDR(balance1);
+			//IDR check
+			if(!idr.isEmpty()&& usd.isEmpty()) {
+			  
+				double idr1 =Double.parseDouble(idr);
+				if(idr1 > 0) {
+					idr2.addValue(idr1);
+					File f2 = new File("profile/"+Main.User+".txt");
+					File file = new File("profile/temp.txt");
 				
-				if(!file.exists()) {
-					try {
-						file.createNewFile();
-						FileWriter Write = new FileWriter(file);
-				
-						Write.write(""+name+"\n"+password+"\n"+ 
+		//Overwrite file	
+					
+					if(!file.exists()) {
+						try {
+							file.createNewFile();
+							FileWriter Write = new FileWriter(file);
+					
+							Write.write(""+name+"\n"+password+"\n"+ 
+									    email + "\n" + brth +
+									    "\n" + address+"\n"+Math.round(idr2.getValue()));
+							Write.close();
+						} 
+						catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+					}
+					
+					else {
+						FileWriter reWrite;
+						try {
+							reWrite = new FileWriter(file,false);
+							reWrite.write(""+name+"\n"+password+"\n"+ 
 								    email + "\n" + brth +
 								    "\n" + address+"\n"+Math.round(idr2.getValue()));
-						Write.close();
-					} 
-					catch (IOException e) {
-						e.printStackTrace();
-					}
-					
-				}
-				
-				else {
-					FileWriter reWrite;
-					try {
-						reWrite = new FileWriter(file,false);
-						reWrite.write(""+name+"\n"+password+"\n"+ 
-							    email + "\n" + brth +
-							    "\n" + address+"\n"+Math.round(idr2.getValue()));
-						System.out.println("ternyata masuk else");
-						reWrite.close();
-					} 
-					catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}	
-					
-				}
-				
-				if(f2.exists()) {
-					FileWriter reWrite;
-					try {
-						reWrite = new FileWriter(f2,false);
-						reWrite.write(""+name+"\n"+password+"\n"+ 
-							    email + "\n" + brth +
-							    "\n" + address+"\n"+Math.round(idr2.getValue()));
-						System.out.println("ternyata masuk else");
-						reWrite.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-							
-				}
-				//overwrite file end
-			}
-			else {
-				JOptionPane.showMessageDialog(frame, "Input yang anda masukan salah","Input Error",JOptionPane.WARNING_MESSAGE);
-			}
-		}	
-		//USD check
-		else if(!usd.isEmpty()) {
-			System.out.println("masuk usd");
-			  double usd1 = Double.parseDouble(usd);
-			  if(usd1 > 0) {
-				  System.out.println("objek usd");
-					USD usd2 = new USD(0);
-					System.out.println("objek ada");
-
-				usd2.addValue(usd1);
-				usd2.toIDR();
-				idr2.addValue(usd2.getValue());
-				System.out.println(usd2.getValue());
-				File f3 = new File("profile/"+Main.User+".txt");
-				File file1 = new File("profile/temp.txt");
-			
-			//overwrite file
-				if (file1.exists()){
-					FileWriter reWrite;
-					try {
-						reWrite = new FileWriter(file1,false);
-						reWrite.write(""+name+"\n"+password+"\n"+ 
-							    email + "\n" + brth +
-							    "\n" + address+"\n"+Math.round(idr2.getValue()));
-						System.out.println("ternyata masuk else");
-						reWrite.close();
+							System.out.println("ternyata masuk else");
+							reWrite.close();
 						} 
-					catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						}				
-				}
-				
-				if(f3.exists()) {
-					FileWriter reWrite;
-					try {
-						reWrite = new FileWriter(f3,false);
-						reWrite.write(""+name+"\n"+password+"\n"+ 
-							    email + "\n" + brth +
-							    "\n" + address+"\n"+Math.round(idr2.getValue()));
-						System.out.println("ternyata masuk else");
-						reWrite.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+						catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}	
+						
 					}
+					
+					if(f2.exists()) {
+						FileWriter reWrite;
+						try {
+							reWrite = new FileWriter(f2,false);
+							reWrite.write(""+name+"\n"+password+"\n"+ 
+								    email + "\n" + brth +
+								    "\n" + address+"\n"+Math.round(idr2.getValue()));
+							System.out.println("ternyata masuk else");
+							reWrite.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+								
+					}
+					//overwrite file end
 				}
-			  }//end usd check
-			}
+				else {
+					JOptionPane.showMessageDialog(frame, "Input yang anda masukan salah","Input Error",JOptionPane.WARNING_MESSAGE);
+				}
+			}	
+			//USD check
+			else if(!usd.isEmpty()) {
+				System.out.println("masuk usd");
+				  double usd1 = Double.parseDouble(usd);
+				  if(usd1 > 0) {
+					  System.out.println("objek usd");
+						USD usd2 = new USD(0);
+						System.out.println("objek ada");
+
+					usd2.addValue(usd1);
+					usd2.toIDR();
+					idr2.addValue(usd2.getValue());
+					System.out.println(usd2.getValue());
+					File f3 = new File("profile/"+Main.User+".txt");
+					File file1 = new File("profile/temp.txt");
+				
+				//overwrite file
+					if (file1.exists()){
+						FileWriter reWrite;
+						try {
+							reWrite = new FileWriter(file1,false);
+							reWrite.write(""+name+"\n"+password+"\n"+ 
+								    email + "\n" + brth +
+								    "\n" + address+"\n"+Math.round(idr2.getValue()));
+							System.out.println("ternyata masuk else");
+							reWrite.close();
+							} 
+						catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+							}				
+					}
+					
+					if(f3.exists()) {
+						FileWriter reWrite;
+						try {
+							reWrite = new FileWriter(f3,false);
+							reWrite.write(""+name+"\n"+password+"\n"+ 
+								    email + "\n" + brth +
+								    "\n" + address+"\n"+Math.round(idr2.getValue()));
+							System.out.println("ternyata masuk else");
+							reWrite.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				  }//end usd check
+				}		
+		}
 		else {
 			JOptionPane.showMessageDialog(frame, "Input yang anda masukan salah","Input Error",JOptionPane.WARNING_MESSAGE);
 		}
+	}
+	
+	@Override
+	public Insets getBorderInsets(Component c) {
+	    return insets;
+	}
+	
+	@Override
+	public Insets getBorderInsets(Component c, Insets insets) {
+	    return getBorderInsets(c);
+	}
+	
+	@Override
+	public void paintBorder(
+	    Component c,
+	    Graphics g,
+	    int x, int y,
+	    int width, int height) {
+	
+	    Graphics2D g2 = (Graphics2D)g;
+	
+	    int bottomLineY = height-thickness-pointerSize;
+	
+	    RoundRectangle2D.Double bubble = new RoundRectangle2D.Double(
+	        0+strokePad,
+	        0+strokePad,
+	        width-thickness,
+	        bottomLineY,
+	        radii,
+	        radii
+	        );
+	
+	    Polygon pointer = new Polygon();
+	
+	    // left point
+	    pointer.addPoint(
+	        strokePad+radii+pointerPad,
+	        bottomLineY);
+	    // right point
+	    pointer.addPoint(
+	        strokePad+radii+pointerPad+pointerSize,
+	        bottomLineY);
+	    // bottom point
+	    pointer.addPoint(
+	        strokePad+radii+pointerPad+(pointerSize/2),
+	        height-strokePad);
+	
+	    Area area = new Area(bubble);
+	    area.add(new Area(pointer));
+	
+	    g2.setRenderingHints(hints);
+	
+	    Area spareSpace = new Area(new Rectangle(0,0,width,height));
+	    spareSpace.subtract(area);
+	    g2.setClip(spareSpace);
+	    g2.clearRect(0,0,width,height);
+	    g2.setClip(null);
+	
+	    g2.setColor(color);
+	    g2.setStroke(stroke);
+	    g2.draw(area);
 	}
 }
