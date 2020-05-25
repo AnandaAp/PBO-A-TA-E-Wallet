7b31cb496f55460d834ab617de8018a870731f55
Yohanesrinto <63203011+yohanesrinto@users.noreply.github.com>
2020-05-25 20:45:16 +0700

    Improve UI and Adding more Input Handling

--- src/Sources/WithdrawBalance.java
+++ src/Sources/WithdrawBalance.java
@@ -1,4 +1,7 @@
 package Sources;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import java.io.BufferedReader;
@@ -19,19 +22,64 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+
 import javax.swing.JTextField;
+import javax.swing.border.AbstractBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.geom.Area;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.event.ActionEvent;
-
-public class WithdrawBalance {
+import java.util.regex.*;
+public class WithdrawBalance extends AbstractBorder{
 
 	public JFrame frame;
 	private JTextField textField;
 	public String name,password, email, brth, address, balance;
-	public WithdrawBalance(UserWallet u ) {
-		initialize(u);
+	private Color color;
+    private int thickness = 4;
+    private int radii = 8;
+    private int pointerSize = 7;
+    private Insets insets = null;
+    private BasicStroke stroke = null;
+    private int strokePad;
+    private int pointerPad = 4;
+    RenderingHints hints;
+    private String numPat = "\\d+";
+    
+    /**
+	 * @wbp.parser.constructor
+	 */
+	public WithdrawBalance(UserWallet u) {
+		this.initialize(u);
 	}
+	public WithdrawBalance(Color color) {
+		this(color, 4, 8, 7);
+	}
+	public WithdrawBalance(
+		    Color color, int thickness, int radii, int pointerSize) {
+	        this.thickness = thickness;
+	        this.radii = radii;
+	        this.pointerSize = pointerSize;
+		    this.color = color;
+		
+		    stroke = new BasicStroke(thickness);
+		    strokePad = thickness/2;
+		
+		    hints = new RenderingHints(
+		        RenderingHints.KEY_ANTIALIASING,
+		        RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		    int pad = radii + strokePad;
+		    int bottomPad = pad + pointerSize + strokePad;
+		    insets = new Insets(pad,pad,bottomPad,pad);
+		}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -56,6 +104,7 @@ public class WithdrawBalance {
 		textField = new JTextField();
 		textField.setColumns(10);
 		textField.setBounds(71, 72, 183, 20);
+		textField.setBorder(new Register(Color.black.darker(),2,6,0));
 		frame.getContentPane().add(textField);
 		
 		//confirm button
@@ -63,6 +112,7 @@ public class WithdrawBalance {
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				WithdrawFunction(textField);
+				JOptionPane.showMessageDialog(frame, "Transaksi Berhasil", "Input Success", JOptionPane.PLAIN_MESSAGE);
 			}
 		});
 		
@@ -120,6 +170,10 @@ public class WithdrawBalance {
 				e.printStackTrace();
 			}
 		
+		Pattern pat = Pattern.compile(numPat);
+		Matcher mat = pat.matcher(balance);
+		boolean ok = mat.matches();
+		if(ok) {
 		//value type double 
 		double value1 = Double.parseDouble(value);
 		double balance1 = Double.parseDouble(balance);
@@ -182,9 +236,71 @@ public class WithdrawBalance {
 			}
 				//overwrite file end
 			}
-			else {
-				JOptionPane.showMessageDialog(frame, "Input yang anda masukan salah","Input Error",JOptionPane.WARNING_MESSAGE);
-			}
-		}	
+		}
+		}
+		else {
+			JOptionPane.showMessageDialog(frame, "Input yang anda masukan salah","Input Error",JOptionPane.WARNING_MESSAGE);
+		}
+	}
+	
+	@Override
+	public Insets getBorderInsets(Component c) {
+	    return insets;
+	}
+	
+	@Override
+	public Insets getBorderInsets(Component c, Insets insets) {
+	    return getBorderInsets(c);
+	}
+	
+	@Override
+	public void paintBorder(
+	    Component c,
+	    Graphics g,
+	    int x, int y,
+	    int width, int height) {
+	
+	    Graphics2D g2 = (Graphics2D)g;
+	
+	    int bottomLineY = height-thickness-pointerSize;
+	
+	    RoundRectangle2D.Double bubble = new RoundRectangle2D.Double(
+	        0+strokePad,
+	        0+strokePad,
+	        width-thickness,
+	        bottomLineY,
+	        radii,
+	        radii
+	        );
+	
+	    Polygon pointer = new Polygon();
+	
+	    // left point
+	    pointer.addPoint(
+	        strokePad+radii+pointerPad,
+	        bottomLineY);
+	    // right point
+	    pointer.addPoint(
+	        strokePad+radii+pointerPad+pointerSize,
+	        bottomLineY);
+	    // bottom point
+	    pointer.addPoint(
+	        strokePad+radii+pointerPad+(pointerSize/2),
+	        height-strokePad);
+	
+	    Area area = new Area(bubble);
+	    area.add(new Area(pointer));
+	
+	    g2.setRenderingHints(hints);
+	
+	    Area spareSpace = new Area(new Rectangle(0,0,width,height));
+	    spareSpace.subtract(area);
+	    g2.setClip(spareSpace);
+	    g2.clearRect(0,0,width,height);
+	    g2.setClip(null);
+	
+	    g2.setColor(color);
+	    g2.setStroke(stroke);
+	    g2.draw(area);
 	}
 }
